b3509014594995ab27a9fe2fa0918f444441f7f6
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-08-31 17:10:41 -0300

    realizando likes nos comentarios

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,11 +1,17 @@
 import { ThumbsUp, Trash } from "@phosphor-icons/react";
 import styles from "./index.module.css";
 import { Avatar } from "../Avatar";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
   function handleDeleteComment() {
     onDeleteComment(content); // esse parametro é mais ou menos para mostra de qual comentario ele ta falando, o correto seria ter o id do comentario
   }
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar src="https://github.com/Erick10Kito.png" hasBorder={false} />
@@ -29,9 +35,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
